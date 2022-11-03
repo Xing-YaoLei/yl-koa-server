06435ae0271499957741e2085d6cf9666a3608bc
Xing_YaoLei <542528512@qq.com>
2022-11-03 22:20:46 +0800

    添加了默认页面

---
+++ src/public/assets/404NotFound.59efa18d.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c}from"./index.b9775775.js";const n={};function t(r,_){return e(),c("div",null,"404NotFound")}const a=o(n,[["render",t]]);export{a as default};
---
+++ src/public/assets/ProfileView.0cfb5f13.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index.b9775775.js";const r={};function n(t,s){return c(),o("div",null,"ProFile")}const a=e(r,[["render",n]]);export{a as default};
